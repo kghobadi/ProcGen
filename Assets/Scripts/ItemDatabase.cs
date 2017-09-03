be70d8a9edbf000c91d396c1521420b771d56eb3
Unknown <ksg304@nyu.edu>
2017-09-03 19:48:39 -0400

    started working on Inventory Tutorial

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using LitJson;
+
+public class ItemDatabase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public class Item
+    {
+
+    }
+	
+}
