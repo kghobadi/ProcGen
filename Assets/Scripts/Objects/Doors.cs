6f45daa5c19ad5654f160817abb90341c5b0ed49
Unknown <ksg304@nyu.edu>
2017-09-14 15:28:37 -0400

    stuff

---
+++ Assets/Scripts/Objects/Doors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : Interactable {
+
+    public int caseIndex;
+    
+
+    public override void handleClickSuccess()
+    {
+        //Load Interior of X place
+        
+    }
+
+
+}
